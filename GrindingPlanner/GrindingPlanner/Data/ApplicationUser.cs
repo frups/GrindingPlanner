c63f942c806411c8ee96cf883d87909d21cda8f8
frups <frupsjk@gmail.com>
2024-07-27 16:32:06 +0200

    Add project files.

---
+++ GrindingPlanner/GrindingPlanner/Data/ApplicationUser.cs
@@ -0,0 +1,10 @@
+using Microsoft.AspNetCore.Identity;
+
+namespace GrindingPlanner.Data
+{
+    // Add profile data for application users by adding properties to the ApplicationUser class
+    public class ApplicationUser : IdentityUser
+    {
+    }
+
+}
