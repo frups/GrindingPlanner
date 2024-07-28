f2708732f06eb13da04f263cde09ea17a964702c
frups <frupsjk@gmail.com>
2024-07-28 16:41:44 +0200

    first db models

---
+++ GrindingPlanner.Shared/TrainingPlan.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.ComponentModel.DataAnnotations;
+using GrindingPlanner.Data;
+
+namespace GrindingPlanner.Shared
+{
+    public class TrainingPlan
+    {
+        [Key]
+        public string TrainingPlanId { get; set; }
+        [Required]
+        public ApplicationUser UserId { get; set; }
+        public ApplicationUser TrainerId { get; set; }
+    }
+}
--- GrindingPlanner/GrindingPlanner/Data/ApplicationDbContext.cs
+++ GrindingPlanner/GrindingPlanner/Data/ApplicationDbContext.cs
@@ -1,9 +1,11 @@
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using GrindingPlanner.Shared;
 
 namespace GrindingPlanner.Data
 {
     public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext<ApplicationUser>(options)
     {
+        public DbSet<TrainingPlan> TrainingPlans { get; set; }
     }
 }
