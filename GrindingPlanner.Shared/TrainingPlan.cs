4d9b57f0fbe01fecd17d7260ad1c8583bddd8b23
frups <frupsjk@gmail.com>
2024-07-31 17:27:37 +0200

    added shared project for models

---
+++ GrindingPlanner.Shared/TrainingPlan.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GrindingPlanner.Shared
+{
+    public class TrainingPlan
+    {
+        public required string TrainingPlanId { get; set; }
+        public string? TrainingPlanName { get; set; }
+        public required string OwnerId { get; set; }
+        public string? TrainerId { get; set; }
+    }
+}
