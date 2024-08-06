9f78df10f891ea57e386225636fc2fc108bc0421
frups <frupsjk@gmail.com>
2024-08-06 11:59:47 +0200

    training plan scaffolded crud

---
+++ GrindingPlanner/GrindingPlanner/Data/Migrations/20240806090335_TrainingPlans.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GrindingPlanner.Migrations
+{
+    /// <inheritdoc />
+    public partial class TrainingPlans : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "OwnerId",
+                table: "TrainingPlan",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "OwnerId",
+                table: "TrainingPlan",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+        }
+    }
+}
