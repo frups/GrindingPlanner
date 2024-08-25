ef5458085fa2480809e90f0cae8ed9d0b06cd07c
frups <frupsjk@gmail.com>
2024-08-25 15:08:09 +0200

    adding new week plans calculations based on week #todo: unit tests and fixes

---
+++ GrindingPlannerUnitTests/DateWeekResolverTest.cs
@@ -0,0 +1,18 @@
+namespace GrindingPlannerUnitTests
+{
+    public class DateWeekResolverTest
+    {
+        private readonly DateWeekResolverTest _dateWeekResolverTest;
+
+        public DateWeekResolverTest()
+        {
+            _dateWeekResolverTest = new DateWeekResolverTest();
+        }
+
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
